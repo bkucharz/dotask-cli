3e1f123fa52a5672ae4a82f1787a68b0ba0caddb
bkucharz <bartosz_kucharz@outlook.com>
2025-07-10 19:35:45 +0200

    Add initial CLI argument parsing

---
+++ dotask/cli.py
@@ -0,0 +1,26 @@
+import argparse
+from dotask.dotask import *
+
+global_parser = argparse.ArgumentParser(
+    prog='test',
+    description="A lightweight command-line tool for managing tasks. Add, update, delete, and track tasks.",
+)
+
+global_parser.add_argument('-f', '--file', default='./dotask/tasks.json', help='file path to tasks')
+
+subparsers = global_parser.add_subparsers(title='actions', help='chose what to do with a task list')
+subparsers.required = True
+
+add_parser = subparsers.add_parser('add', help='add a new task')
+
+update_parser = subparsers.add_parser('update', help='update a task')
+
+delete_parser = subparsers.add_parser('delete', help='delete a task')
+
+list_parser = subparsers.add_parser('list', help='list tasks')
+
+args = global_parser.parse_args()
+
+
+
+
